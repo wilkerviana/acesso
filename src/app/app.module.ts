fcec13441455501f3d397f9a5bdb14a599578dd7
Wilker Andrade Viana <wilker.info@gmail.com>
2018-10-03 14:27:51 -0300

    Initial commit with footer component style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
